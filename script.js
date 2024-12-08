eb2925cb022abb0c539e19920185d6776b20918f
Gonzalo Brunoldi Stasiow <62858745+gonzalobrunoldis@users.noreply.github.com>
2024-12-08 20:43:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPage = window.location.pathname.split('/').pop();
+    const navItems = document.querySelectorAll('.nav-item');
+    
+    navItems.forEach(item => {
+        const href = item.getAttribute('href');
+        if (href === currentPage) {
+            item.classList.add('active');
+        }
+    });
+}); 
